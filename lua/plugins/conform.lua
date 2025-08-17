b91d8acfb52e8ab55f8127f25a754263f245d080
Norbaev <timnrbv@gmail.com>
2025-08-17 23:15:22 +0500

    add organize imports!

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -3,7 +3,10 @@ return {
 		"stevearc/conform.nvim",
 		opts = {},
 		config = function()
-			require("conform").setup({
+			local conform = require("conform")
+
+			-- настройка форматтеров
+			conform.setup({
 				formatters_by_ft = {
 					lua = { "stylua" },
 					javascript = { "prettier" },
@@ -16,14 +19,37 @@ return {
 					markdown = { "prettier" },
 				},
 			})
+
+			-- автокоманда для форматирования
 			vim.api.nvim_create_autocmd("BufWritePre", {
 				pattern = "*",
 				callback = function(args)
-					require("conform").format({
+					conform.format({
 						bufnr = args.buf,
+						async = false,
 					})
 				end,
 			})
+
+			-- автокоманда для organizeImports
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = vim.api.nvim_create_augroup("AutoOrganizeImports", {}),
+				pattern = { "*.ts", "*.tsx", "*.js", "*.jsx", "*.vue" },
+				callback = function()
+					vim.lsp.buf.code_action({
+						apply = true,
+						context = { only = { "source.organizeImports" }, diagnostics = {} },
+					})
+				end,
+			})
+
+			-- 🔑 клавиша для ручного organizeImports
+			vim.keymap.set("n", "<leader>co", function()
+				vim.lsp.buf.code_action({
+					apply = true,
+					context = { only = { "source.organizeImports" }, diagnostics = {} },
+				})
+			end, { desc = "Organize Imports" })
 		end,
 	},
 }
