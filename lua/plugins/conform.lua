84dbbb6ace1ac74c695b40a06482e73a986c5252
Norbaev <timnrbv@gmail.com>
2025-08-17 21:29:29 +0500

    add prettier for markdown

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,26 +1,29 @@
-return {{
-    "stevearc/conform.nvim",
-    opts = {},
-    config = function()
-        require("conform").setup({
-            formatters_by_ft = {
-                lua = {"stylua"},
-                javascript = {"prettier"},
-                typescript = {"prettier"},
-                javascriptreact = {"prettier"},
-                typescriptreact = {"prettier"},
-                vue = {"prettier"},
-                css = {"prettier"},
-                scss = {"prettier"}
-            }
-        })
-        vim.api.nvim_create_autocmd("BufWritePre", {
-            pattern = "*",
-            callback = function(args)
-                require("conform").format({
-                    bufnr = args.buf
-                })
-            end
-        })
-    end
-}}
+return {
+	{
+		"stevearc/conform.nvim",
+		opts = {},
+		config = function()
+			require("conform").setup({
+				formatters_by_ft = {
+					lua = { "stylua" },
+					javascript = { "prettier" },
+					typescript = { "prettier" },
+					javascriptreact = { "prettier" },
+					typescriptreact = { "prettier" },
+					vue = { "prettier" },
+					css = { "prettier" },
+					scss = { "prettier" },
+					markdown = { "prettier" },
+				},
+			})
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				pattern = "*",
+				callback = function(args)
+					require("conform").format({
+						bufnr = args.buf,
+					})
+				end,
+			})
+		end,
+	},
+}
