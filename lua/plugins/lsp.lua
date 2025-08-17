5332370c5bfd6bb952ef073fe929a66f20a2d8d7
Norbaev <timnrbv@gmail.com>
2025-08-17 11:23:59 +0500

    add: vtsls

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,22 +2,43 @@ return {
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
+			local vue_language_server_path = vim.fn.stdpath("data")
+				.. "/mason/packages/vue-language-server/node_modules/@vue/language-server"
+			local vue_plugin = {
+				name = "@vue/typescript-plugin",
+				location = vue_language_server_path,
+				languages = { "vue" },
+				configNamespace = "typescript",
+			}
+
+			local ts_filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" }
+
+			vim.lsp.config("vtsls", {
+				settings = {
+					vtsls = {
+						tsserver = {
+							globalPlugins = { vue_plugin },
+						},
+					},
+				},
+				filetypes = ts_filetypes,
+			})
+
 			-- Регистрация LSP-серверов через vim.lsp.config
 			vim.lsp.config("lua_ls", {})
-			vim.lsp.config("ts_ls", {})
 			vim.lsp.config("vue_ls", {})
 
 			-- Автоматическое включение LSP
-			vim.lsp.enable("lua_ls")
-			vim.lsp.enable("ts_ls")
-			vim.lsp.enable("vue_ls")
+			vim.lsp.enable({ "lua_ls", "vue_ls", "vtsls" })
 
 			-- Автокоманды при attach LSP
 			vim.api.nvim_create_autocmd("LspAttach", {
 				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
 				callback = function(ev)
 					vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
-					local opts = { buffer = ev.buf }
+					local opts = {
+						buffer = ev.buf,
+					}
 
 					-- Клавиши LSP
 					vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
@@ -25,10 +46,15 @@ return {
 					vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
 					vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
 					vim.keymap.set("n", "<Leader>D", vim.lsp.buf.type_definition, opts)
-					vim.keymap.set("n", "<Leader>lr", vim.lsp.buf.rename, { buffer = ev.buf, desc = "Rename Symbol" })
+					vim.keymap.set("n", "<Leader>lr", vim.lsp.buf.rename, {
+						buffer = ev.buf,
+						desc = "Rename Symbol",
+					})
 					vim.keymap.set({ "n", "v" }, "<Leader>la", vim.lsp.buf.code_action, opts)
 					vim.keymap.set("n", "<Leader>lf", function()
-						vim.lsp.buf.format({ async = true })
+						vim.lsp.buf.format({
+							async = true,
+						})
 					end, opts)
 				end,
 			})
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,29 +1,22 @@
-return {
-	{
-		"mason-org/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "ts_ls", "html" },
-			})
-		end,
-	},
-	{
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-		dependencies = { "williamboman/mason.nvim" },
-		config = function()
-			require("mason-tool-installer").setup({
-				ensure_installed = {
-					"stylua",
-					"prettier",
-				},
-				auto_update = true,
-			})
-		end,
-	},
-}
+return {{
+    "williamboman/mason.nvim",
+    config = function()
+        require("mason").setup()
+    end
+}, {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+        require("mason-lspconfig").setup({
+            ensure_installed = {"lua_ls", "vue_ls", "vtsls", "html"}
+        })
+    end
+}, {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    dependencies = {"williamboman/mason.nvim"},
+    config = function()
+        require("mason-tool-installer").setup({
+            ensure_installed = {"stylua", "prettier"},
+            auto_update = true
+        })
+    end
+}}
