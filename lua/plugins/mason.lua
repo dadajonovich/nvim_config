98f2cbfbf5e4b434a33c009038cee25b31a67375
Norbaev <timnrbv@gmail.com>
2025-08-20 00:09:53 +0500

    add css lsp

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,22 +1,35 @@
-return {{
-    "williamboman/mason.nvim",
-    config = function()
-        require("mason").setup()
-    end
-}, {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-        require("mason-lspconfig").setup({
-            ensure_installed = {"lua_ls", "vue_ls", "vtsls", "html", "stylelint_lsp"}
-        })
-    end
-}, {
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-    dependencies = {"williamboman/mason.nvim"},
-    config = function()
-        require("mason-tool-installer").setup({
-            ensure_installed = {"stylua", "prettier"},
-            auto_update = true
-        })
-    end
-}}
+return {
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"vue_ls",
+					"vtsls",
+					"html",
+					"stylelint_lsp",
+					"cssls",
+					"css_variables",
+					"cssmodules_ls",
+				},
+			})
+		end,
+	},
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		dependencies = { "williamboman/mason.nvim" },
+		config = function()
+			require("mason-tool-installer").setup({
+				ensure_installed = { "stylua", "prettier" },
+				auto_update = true,
+			})
+		end,
+	},
+}
