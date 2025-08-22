8e2cc414b802cd3bfd81fbd9c7b9c4d093ac4b4c
Norbaev <timnrbv@gmail.com>
2025-08-22 10:40:48 +0500

    upgdate keymap for gitsigns, use my fn map for multicursor

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -26,10 +26,42 @@ return {
 					end, "Previous git hunk")
 
 					-- Actions
+					map("n", "<leader>gs", gitsigns.stage_hunk, "Stage hunk")
+					map("n", "<leader>gr", gitsigns.reset_hunk, "Reset hunk")
+
+					map("v", "<leader>gs", function()
+						gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+					end, "Stage hunk (visual)")
+
+					map("v", "<leader>gr", function()
+						gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+					end, "Reset hunk (visual)")
+
 					map("n", "<leader>gS", gitsigns.stage_buffer, "Stage entire buffer")
 					map("n", "<leader>gR", gitsigns.reset_buffer, "Reset entire buffer")
 					map("n", "<leader>gp", gitsigns.preview_hunk, "Preview hunk")
+					map("n", "<leader>gi", gitsigns.preview_hunk_inline, "Preview hunk inline")
+
+					map("n", "<leader>gb", function()
+						gitsigns.blame_line({ full = true })
+					end, "Git blame line (full)")
+
 					map("n", "<leader>gd", gitsigns.diffthis, "Diff this buffer")
+					map("n", "<leader>gD", function()
+						gitsigns.diffthis("~")
+					end, "Diff this buffer (against ~)")
+
+					map("n", "<leader>gQ", function()
+						gitsigns.setqflist("all")
+					end, "Populate quickfix list (all hunks)")
+
+					map("n", "<leader>gq", gitsigns.setqflist, "Populate quickfix list (open hunks)")
+
+					-- Toggles
+					map("n", "<leader>gB", gitsigns.toggle_current_line_blame, "Toggle line blame")
+					map("n", "<leader>gW", gitsigns.toggle_word_diff, "Toggle word diff")
+
+					-- Text object
 					map({ "o", "x" }, "ig", gitsigns.select_hunk, "Select hunk (inner git)")
 				end,
 			})
--- lua/plugins/multicursor.lua
+++ lua/plugins/multicursor.lua
@@ -5,56 +5,58 @@ return {
 		local mc = require("multicursor-nvim")
 		mc.setup()
 
-		local set = vim.keymap.set
+		local map = require("core.mappings").map
 
-		-- Add or skip cursor above/below the main cursor.
-		set({ "n", "x" }, "<up>", function()
+		-- Add or skip cursor above/below the main cursor
+		map({ "n", "x" }, "<up>", function()
 			mc.lineAddCursor(-1)
-		end)
-		set({ "n", "x" }, "<down>", function()
+		end, "Add cursor above")
+
+		map({ "n", "x" }, "<down>", function()
 			mc.lineAddCursor(1)
-		end)
-		set({ "n", "x" }, "<leader><up>", function()
+		end, "Add cursor below")
+
+		map({ "n", "x" }, "<leader><up>", function()
 			mc.lineSkipCursor(-1)
-		end)
-		set({ "n", "x" }, "<leader><down>", function()
+		end, "Skip cursor above")
+
+		map({ "n", "x" }, "<leader><down>", function()
 			mc.lineSkipCursor(1)
-		end)
+		end, "Skip cursor below")
 
 		-- Add or skip adding a new cursor by matching word/selection
-		set({ "n", "x" }, "<leader>n", function()
+		map({ "n", "x" }, "<leader>n", function()
 			mc.matchAddCursor(1)
-		end)
-		set({ "n", "x" }, "<leader>s", function()
+		end, "Add cursor next match")
+
+		map({ "n", "x" }, "<leader>s", function()
 			mc.matchSkipCursor(1)
-		end)
-		set({ "n", "x" }, "<leader>N", function()
+		end, "Skip cursor next match")
+
+		map({ "n", "x" }, "<leader>N", function()
 			mc.matchAddCursor(-1)
-		end)
-		set({ "n", "x" }, "<leader>S", function()
+		end, "Add cursor prev match")
+
+		map({ "n", "x" }, "<leader>S", function()
 			mc.matchSkipCursor(-1)
-		end)
+		end, "Skip cursor prev match")
 
-		-- Add and remove cursors with control + left click.
-		set("n", "<c-leftmouse>", mc.handleMouse)
-		set("n", "<c-leftdrag>", mc.handleMouseDrag)
-		set("n", "<c-leftrelease>", mc.handleMouseRelease)
+		-- Add and remove cursors with control + left click
+		map("n", "<c-leftmouse>", mc.handleMouse, "Add cursor with mouse")
+		map("n", "<c-leftdrag>", mc.handleMouseDrag, "Drag to add cursors")
+		map("n", "<c-leftrelease>", mc.handleMouseRelease, "Release mouse cursor")
 
-		-- Disable and enable cursors.
-		set({ "n", "x" }, "<c-q>", mc.toggleCursor)
+		-- Disable and enable cursors
+		map({ "n", "x" }, "<c-q>", mc.toggleCursor, "Toggle cursor enable")
 
-		-- Mappings defined in a keymap layer only apply when there are
-		-- multiple cursors. This lets you have overlapping mappings.
-		mc.addKeymapLayer(function(layerSet)
-			-- Select a different cursor as the main one.
-			layerSet({ "n", "x" }, "<left>", mc.prevCursor)
-			layerSet({ "n", "x" }, "<right>", mc.nextCursor)
+		-- Keymap layer (only when multiple cursors are active)
+		mc.addKeymapLayer(function(layerMap)
+			layerMap({ "n", "x" }, "<left>", mc.prevCursor)
+			layerMap({ "n", "x" }, "<right>", mc.nextCursor)
 
-			-- Delete the main cursor.
-			layerSet({ "n", "x" }, "<leader>x", mc.deleteCursor)
+			layerMap({ "n", "x" }, "<leader>x", mc.deleteCursor)
 
-			-- Enable and clear cursors using escape.
-			layerSet("n", "<esc>", function()
+			layerMap("n", "<esc>", function()
 				if not mc.cursorsEnabled() then
 					mc.enableCursors()
 				else
@@ -63,7 +65,7 @@ return {
 			end)
 		end)
 
-		-- Customize how cursors look.
+		-- Customize how cursors look
 		local hl = vim.api.nvim_set_hl
 		hl(0, "MultiCursorCursor", { reverse = true })
 		hl(0, "MultiCursorVisual", { link = "Visual" })
