72f63660edf086c1aa4abb788cd689f131f1c282
Norbaev <timnrbv@gmail.com>
2025-09-02 18:12:37 +0500

    foldenable

--- lua/core/configs.lua
+++ lua/core/configs.lua
@@ -5,7 +5,7 @@ vim.opt.mouse = "a"
 vim.opt.mousefocus = true
 
 vim.schedule(function()
-    vim.o.clipboard = 'unnamedplus'
+	vim.o.clipboard = "unnamedplus"
 end)
 
 vim.opt.shiftwidth = 4
@@ -15,3 +15,7 @@ vim.opt.softtabstop = 4
 vim.opt.scrolloff = 8
 vim.opt.wrap = false
 vim.opt.termguicolors = true
+
+vim.o.foldmethod = "expr"
+vim.o.foldexpr = "nvim_treesitter#foldexpr()"
+vim.o.foldlevel = 99
